a32389d517b989e88cccfabcb2f5e613a5886cc5
AveshMishra20 <aveshmishra1811@gmail.com>
2024-10-21 21:35:10 +0530

    video streaming platform

--- src/uitils/constants.js
+++ src/uitils/constants.js
@@ -1,6 +1,5 @@
 export const API_Key="AIzaSyBPKKNmqQHfJdSDgibpwJhnzyRTY2q8l5o";
 
-
 export const Youtube_Videos_Api="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+API_Key;
 
 export const Youtube_Videos_Comment_Api="https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=";
@@ -11,3 +10,6 @@ export const RecommendedVideos_API="https://youtube.googleapis.com/youtube/v3/pl
 export const VideosCategories_API="https://youtube.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=IN&key="+API_Key;
 
 export const Search_Suggestion_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+//https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=
+//AIzaSyBPKKNmqQHfJdSDgibpwJhnzyRTY2q8l5o
